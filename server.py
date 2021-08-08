a1424cd4871139494e54e834776e58f1551ec38a
amogh0503 <amoghagarwal0503@gmail.com>
2021-08-08 23:32:08 +0530

    args added

--- server.py
+++ server.py
@@ -5,5 +5,17 @@ app = Flask(__name__)
 def welcome():
     input_data = request.json["Name"]
     return input_data
+
+@app.route('/v1/send_cert',methods=['GET'])
+def get_params():       # only accepts first parameter of url paramaters
+    queryString = request.args  #For Signature Given
+    Title = request.args.get('Title')
+    Name = request.args.get('Name')
+    Body = request.args.get('Body')
+    Date = request.args.get('Date')
+    Signature = request.args.get('Signature',None)  #optional parameter in url
+    Link = request.args.get('Link')
+    return queryString
+
 if __name__ == '__main__':
-    app.run(host='localhost', port=105)
+    app.run(host='localhost',debug=True, port=105)
